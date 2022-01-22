46ecce60ce789dd105e912c1eecf31c3aa44d87e
devs <devs@hsk-services.com>
2022-01-22 21:30:15 +0500

    add queries

--- app/models/queries.js
+++ app/models/queries.js
@@ -1,11 +1,17 @@
 var queries = {
     insertProvider: `INSERT INTO 
-                    service.provider(
-                    first_name, last_name, middle_name, work_radius, status, cnic, username, date_of_birth, photo, phone_number_1, phone_number_2, brief_description, detailed_description)
+                    service.provider
+                    (first_name, last_name, middle_name, work_radius, status, cnic, username, date_of_birth, photo, phone_number_1, phone_number_2, brief_description, detailed_description)
                     VALUES (?, ?, ?, IFNULL(?,DEFAULT(work_radius)), IFNULL(?,DEFAULT(status)), ?, ?, ?, IFNULL(?,DEFAULT(photo)), ?, IFNULL(?,DEFAULT(phone_number_2)), ?, IFNULL(?,DEFAULT(detailed_description)));`,
-    insertCity: `INSERT INTO service.city values(?);`,
-    insertState: `INSERT INTO service.state values(?);`,
-    insertCountry: `INSERT INTO service.country values(?);`,
+    insertCity:     `INSERT IGNORE INTO service.city(city) VALUES(?);`,
+    insertState:    `INSERT IGNORE INTO service.state(state) VALUES(?);`,
+    insertCountry:  `INSERT IGNORE INTO service.country(country) VALUES(?);`,
+    insertAddress:  `INSERT INTO service.provider_address
+                    (provider_id, type, address_row_1, address_row_2, address_row_3, postal_code, latitude, longitude, city, state, country)
+                    VALUES
+                    (?, ?, ?, ?, ?, ?, POINT(?, ?), POINT(?, ?),
+                    (SELECT city.id FROM service.city WHERE city.city=?),
+                    (SELECT state.id FROM service.state WHERE state.state=?),
+                    (SELECT country.id FROM service.country WHERE country.country=?));`,
 };
-
 module.exports = queries;
