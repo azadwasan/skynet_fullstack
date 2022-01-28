aca212f5581c3678c5068dd78e550eb34fb21278
devs <devs@hsk-services.com>
2022-01-28 14:32:54 +0500

    Add queries for specific provider and provider reviews

--- app/models/queries.js
+++ app/models/queries.js
@@ -18,8 +18,16 @@ var queries = {
                     (SELECT state.id FROM service.state WHERE state.state=?),
                     (SELECT country.id FROM service.country WHERE country.country=?));`,
     selectAllProviders: `SELECT * from provider;`,
+    findProviderById: `SELECT service.provider.*, service.provider_address.*, service.city.city, service.state.state, service.country.country FROM service.provider
+                        INNER JOIN service.provider_address ON service.provider.id=provider_address.provider_id
+                        INNER JOIN service.city ON service.provider_address.city = service.city.id
+                        INNER JOIN service.state ON service.provider_address.state = service.state.id
+                        INNER JOIN service.country ON service.provider_address.country = service.country.id
+                        WHERE provider.id=?;`,
+    findReviewById: `SELECT * FROM service.provider_review WHERE provider_review.provider_id=?;`,
+    createReview: `INSERT INTO service.provider_review (provider_id, overall, behavior, time, service, review) VALUES (?, ?, ?, ?, ?, ?);`,
 
-    insertServices: `INSERT INTO  service.services_list (service) VALUES (?);`,
+    insertServices: `INSERT INTO service.services_list (service) VALUES (?);`,
     selectAllServices: `SELECT * FROM service.services_list;`,
 };
 module.exports = queries;
