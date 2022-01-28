5c6af2d9232bdab74dc4447a69e3f900ffc9ae10
devs <devs@hsk-services.com>
2022-01-28 14:27:06 +0500

    Add routes for specific providers and provider reviews

--- app/routes/providers.routes.js
+++ app/routes/providers.routes.js
@@ -9,5 +9,10 @@ module.exports = app =>{
     //Retrieve all tutorials
     router.get("/", provider.findAll);
 
+    router.get("/:id", provider.findOne);
+
+    router.get("/:id/reviews", provider.findOneReview);
+    router.post("/:id/reviews", provider.createReview);
+
     app.use('/api/providers', router);
 };
